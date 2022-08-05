6803eb2a56fb53736fed7d223d6f4ba18ddb8030
IhsanSabri <ihsansabrisenturk@gmail.com>
2022-08-06 01:15:11 +0300

    Folder System Fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import "./App.css";
-import Test from "./components/Test";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Test />
+      <Home />
     </div>
   );
 }
