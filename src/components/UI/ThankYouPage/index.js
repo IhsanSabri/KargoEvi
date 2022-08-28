c25bfd919156ed6593d2a777f8efdf639aa4f593
IhsanSabri <ihsansabrisenturk@gmail.com>
2022-08-28 15:56:50 +0300

    Thank you page

--- src/components/UI/ThankYouPage/index.js
+++ src/components/UI/ThankYouPage/index.js
@@ -3,23 +3,57 @@ import React from "react";
 import { Layout } from "antd";
 import { Box } from "rebass";
 
-import { MainTitle } from "./style";
+import thankYouPage from "../../../assests/thankYouPage.png";
+
+import {
+  MainTitle,
+  OrderInfo,
+  DeliveryInfo,
+  CopyButton,
+  CopyIcon,
+  MainContent,
+  OrderText,
+} from "./style";
 
 const ThankYou = () => {
   return (
     <>
-      <Layout style={{ padding: "50px 50px 0", alignItems: "center" }}>
-        <MainTitle><b>Tebrikler!</b> Siparişiniz Oluşturuldu.</MainTitle>
-        <Box
-          width={2 / 3}
-          style={{
-            minHeight: "280px",
-            padding: "24px",
-            background: "#fff",
-          }}
-        >
-          <div className="site-layout-content">Content</div>
-        </Box>
+      <Layout
+        style={{
+          padding: "50px 50px 50px",
+          alignItems: "center",
+          overflow: "scroll"
+        }}
+      >
+        <MainTitle>
+          <b>Tebrikler!</b> Siparişiniz Oluşturuldu.
+        </MainTitle>
+        <MainContent width={2 / 3}>
+          <OrderInfo width={1 / 2}>
+            <DeliveryInfo>
+              <Box opacity="0.6" fontSize="12px" lineHeight="16px">
+                Kargo Takip Numarası
+              </Box>
+              <Box opacity="0.95" fontSize="32px" lineHeight="44px">
+                35467-34567
+              </Box>
+            </DeliveryInfo>
+            <CopyButton>
+              <CopyIcon transform={"scaleX(-1)"} />
+              Kopyala
+            </CopyButton>
+          </OrderInfo>
+          <OrderText>
+            Odeme yapildiktan sonra kullaniciya kargosunu teslim etmesi icin
+            kargo takip numarasi, kargoyu bizim yurtdisi yapabilmemiz icin
+            airwaybill ( konsimento) ile proforma ve son olarak pazaryerlerinde
+            satis yapan kullanicilar icin gonderisini pazaryerine entegre etmesi
+            icin imkan vermeliyiz. Bilgi amacli olarak; ‘’urun icerik bilgileri
+            ‘’ ve ‘’ alici bilgileri’’ nin girildigi alandaki datalarin bir
+            kismini proforma, bir kisminida airwaybill uzerinden kullanacagiz.
+          </OrderText>
+          <img src={thankYouPage}></img>
+        </MainContent>
       </Layout>
     </>
   );
--- src/components/UI/ThankYouPage/style.js
+++ src/components/UI/ThankYouPage/style.js
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled";
 
+import { Flex } from "rebass";
+import { CopyOutlined } from "@ant-design/icons";
+
 const MainTitle = styled.div`
   font-family: "Manrope";
   font-style: normal;
@@ -10,4 +13,73 @@ const MainTitle = styled.div`
   padding-bottom: 16px;
 `;
 
-export { MainTitle };
+const MainContent = styled(Flex)`
+  font-family: "Manrope";
+  font-style: normal;
+  align-item: center;
+  justify-content: center;
+  min-height: 280px;
+  padding: 56px;
+  background: #fff;
+  flex-direction: column;
+  align-items: center;
+  background: #ffffff;
+  border: 1px solid #e7e7e7;
+  box-shadow: 0px 0px 8px rgba(38, 43, 55, 0.2);
+  border-radius: 8px;
+`;
+
+const OrderInfo = styled(Flex)`
+  background: #f9fbfd;
+  height: 104px;
+  border: 1px dashed #e7e7e7;
+  border-radius: 4px;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const DeliveryInfo = styled(Flex)`
+  flex-direction: column;
+  justify-content: center;
+  font-family: "Manrope";
+  font-style: normal;
+  color: #000000;
+  font-weight: 600;
+  text-align: left;
+`;
+
+const CopyButton = styled(Flex)`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 25px;
+  color: rgb(34, 83, 134);
+  align-items: center;
+  cursor: pointer;
+  text-decoration: unset;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const CopyIcon = styled(CopyOutlined)`
+  padding-right: 7px;
+`;
+
+const OrderText = styled(Flex)`
+  padding: 70px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+`;
+
+export {
+  MainTitle,
+  OrderInfo,
+  DeliveryInfo,
+  CopyButton,
+  CopyIcon,
+  MainContent,
+  OrderText,
+};
